e5952d7c3dc9b23b3c1c0bab0393019d7213416b
madmann8 <madmann686@gmail.com>
2016-05-09 20:28:00 -0400

    Golang Practice with book

--- 02_Structure/popcount/main.go
+++ 02_Structure/popcount/main.go
@@ -1,6 +1,5 @@
 package popcount
 
-// pc[i] is the population count of i.
 var pc [256]byte
 
 func init() {
@@ -9,7 +8,6 @@ func init() {
 	}
 }
 
-// PopCount returns the population count (number of set bits) of x.
 func PopCount(x uint64) int {
 	return int(pc[byte(x>>(0*8))] +
 	pc[byte(x>>(1*8))] +
