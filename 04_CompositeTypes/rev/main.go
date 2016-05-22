f2a80c2844f860f21df41900d7193b70a5030c93
madmann8 <madmann686@gmail.com>
2016-05-21 23:49:23 -0400

    First Commit

--- 04_CompositeTypes/rev/main.go
+++ 04_CompositeTypes/rev/main.go
@@ -3,18 +3,19 @@ package main
 import "fmt"
 
 func main(){
-	s:= [...]int{1,2,3}
+	s= int{1,2,3}
 	rev(s[:])
 	fmt.Println(s)
 }
 
 func rev(input []int) []int {
+	var g []int
 	var j int
 	l:=len(input)-1
 	for i:=0; i<l;i++ {
 		j= input[len(input)-i]
-		input=append(input, j)
+		g=append(input, j)
 		i++
 	}
-	return input[:l]
+	return g[:]
 }
---
+++ 04_CompositeTypes/uni/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := []byte("v")
+	fmt.Println(v)
+	/*fmt.Printf("%v",squash(v))
+}
+
+func squash(b []byte) []int{
+	var k []int
+	for i,n:= range b{
+		if b[i]==b[i+1]{
+			k=append(k,n)
+		}
+		i++
+	}
+	return k
+}
+*/
+	}
